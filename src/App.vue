0403ce505298d384761e1c21fbde7fca3f909c70
fahmiduldul <afahmi13@live.com>
2021-07-16 14:58:13 +0700

    fix: remove unnecessary codes, components and views

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <template>
   <navbar />
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view/>
-  <poll :pollId="'tvs'" />
 </template>
 
 <script lang="ts">
 import navbar from '@/components/Navbar.vue'
 import {defineComponent} from 'vue'
-import poll from '@/components/poll/Poll.vue'
 
 export default defineComponent({
-  components:{navbar, poll}
+  components:{navbar}
 })
 </script>
 
