905cc0605eaa6ced3cb51d1ec863976c5e5f4803
fahmiduldul <afahmi13@live.com>
2021-07-16 14:55:54 +0700

    feat: add link to github and linkedin in navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
-          <img src="@/assets/logo.png" alt="" width="24" height="24">
+          <img src="@/assets/logo.png" alt="" width="35" height="35">
         </a>
 
         <!-- navbar button for mobile -->
@@ -14,11 +14,51 @@
         <!-- everything inside here will be collapsed on mobile -->
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-          <!-- each item or link is a li inside here -->
+          <!--left side of navbar-->
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <nav-vue-router :to="'/'">Home</nav-vue-router>
 
             <nav-vue-router :to="'/auth'">Login/Signup</nav-vue-router>
+          </ul>
+
+          <!--right side og navbar-->
+          <ul class="navbar-nav ms-auto">
+
+            <!--linkedin-->
+            <li class="nav-item">
+              <a href="https://www.linkedin.com/in/fahmi-abdulaziz/" class="nav-link">
+                <img src="../assets/linkedin-logo.svg" width="30" height="30">
+              </a>
+            </li>
+
+            <!--this could be another component, but i'll do it later-->
+            <!--github-->
+            <li class="nav-item dropdown">
+
+              <!--dropdown toggle icon-->
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="../assets/github-logo.svg" width="30" height="30">
+              </a>
+
+              <!--dropdown items-->
+              <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                <li>
+                  <a class="dropdown-item" href="https://github.com/fahmiduldul/tvs_backend">
+                    Project Backend
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="https://github.com/fahmiduldul/tvs_frontend">
+                    Project Frontend
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="https://github.com/fahmiduldul/portfolio">
+                    My Other Project
+                  </a>
+                </li>
+              </ul>
+            </li>
 
           </ul>
 
